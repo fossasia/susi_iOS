d7af5d1e10006986c080e451c5f7f9422b3165c0
Chashmeet Singh <chashmeetsingh@gmail.com>
2017-03-15 20:36:31 +0530

    remove unnecessary dependencies

--- Pods/Target Support Files/Pods-Susi/Pods-Susi-frameworks.sh
+++ Pods/Target Support Files/Pods-Susi/Pods-Susi-frameworks.sh
@@ -90,7 +90,6 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Material/Material.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Popover/Popover.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
@@ -100,7 +99,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Material/Material.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Popover/Popover.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
