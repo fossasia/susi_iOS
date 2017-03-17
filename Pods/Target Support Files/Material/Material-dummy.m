dc01209f7f8e62f335d2b08315945f9c53b246ff
Chashmeet Singh <chashmeetsingh@gmail.com>
2017-03-17 12:46:14 +0530

    Remove Pods Folder

--- Pods/Target Support Files/Material/Material-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Material : NSObject
-@end
-@implementation PodsDummy_Material
-@end
--- Pods/Target Support Files/Toast-Swift/Toast-Swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Toast_Swift : NSObject
-@end
-@implementation PodsDummy_Toast_Swift
-@end
