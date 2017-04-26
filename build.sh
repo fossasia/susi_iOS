b660cb980ae89174ad62e30347047ef85f6c5a86
Chashmeet Singh <chashmeetsingh@gmail.com>
2017-04-26 18:43:54 +0530

    fixes #32: skip codesigning

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-xcodebuild test -workspace Susi.xcworkspace -scheme Susi -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0'
+xcodebuild clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO test -workspace Susi.xcworkspace -scheme Susi -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0'
 
