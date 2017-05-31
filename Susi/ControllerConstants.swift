68ce8823ea61b27a4d46f3385b77d5fe1f0cd346
Ishaan Kakkar <ishaankakkar95@gmail.com>
2017-06-01 03:39:43 +0530

    Added Logic for wallpaper selection from gallery, and a dialog to clear wallpaper, fixed changes requested (#77)

---
+++ Susi/ControllerConstants.swift
@@ -0,0 +1,18 @@
+//
+//  ControllerConstants.swift
+//  Susi
+//
+//  Created by ishaan on 31/05/17.
+//  Copyright © 2017 FOSSAsia. All rights reserved.
+//
+
+import Foundation
+
+struct ControllerConstants {
+    static let errorDialogTitle = "Image Pick Error"
+    static let errorDialogMessage = "Cannot access photos in your albums"
+    static let wallpaperOptionsTitle = "Set Wallpaper"
+    static let wallpaperOptionsPickAction = "Pick image from gallery"
+    static let wallpaperOptionsNoWallpaperAction = "No wallpaper"
+    static let dialogCancelAction = "Cancel"
+}
