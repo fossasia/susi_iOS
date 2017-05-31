6b621d1cf5fadd8d7baaae98a57e59dee5b77407
ishaan <ishaankakkar95@gmail.com>
2017-05-31 22:26:35 +0530

    Added Logic for wallpaper selection from gallery, and a dialog to clear wallpaper, fixed changes requested

--- Susi/MainViewController.swift
+++ Susi/MainViewController.swift
@@ -12,10 +12,9 @@ import Popover
 import ALTextInputBar
 import CoreLocation
 
-class MainViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, ALTextInputBarDelegate, CLLocationManagerDelegate {
+class MainViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, ALTextInputBarDelegate, CLLocationManagerDelegate, UIImagePickerControllerDelegate {
 
-    let cellId = "cellId"
-
+    let cellId = "cellId", userDefaultsWallpaperKey = "wallpaper"
     var messages: [Message] = []
 
     fileprivate var popover: Popover!
@@ -47,7 +46,8 @@ class MainViewController: UICollectionViewController, UICollectionViewDelegateFl
 
     // Location Manager
     var locationManager = CLLocationManager()
-
+    // Image Picker Controller
+    var imagePicker = UIImagePickerController()
     let blackView = UIView()
 
     // Youtube Player
@@ -175,11 +175,15 @@ class MainViewController: UICollectionViewController, UICollectionViewDelegateFl
 
     // Setup Collection View
     func setupCollectionView() {
+        // Check if user defaults have an image, set background as image
+        if let userDefaultData = getWallpaperFromUserDefaults() {
+            if let imageData = userDefaultData as? Data { // Check if object saved in user defaults if of type Data
+                setBackgroundImage(image: UIImage(data : imageData))
+            }
+        }
         collectionView?.backgroundColor = .clear
         collectionView?.delegate = self
-
-        collectionView?.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height - 50)
-
+        collectionView?.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height - 45)
         collectionView?.register(ChatMessageCell.self, forCellWithReuseIdentifier: cellId)
     }
 
@@ -423,23 +427,124 @@ class MainViewController: UICollectionViewController, UICollectionViewDelegateFl
         }
     }
 
+    // Show image picker to set/reset wallpaper
+    func showImagePicker() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.savedPhotosAlbum) {
+            imagePicker.allowsEditing = false
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.savedPhotosAlbum
+            self.present(imagePicker, animated: true, completion: nil)
+        } else {
+            // Show error dialog if no photo is available in photo album
+            let errorDialog = UIAlertController(title: ControllerConstants.errorDialogTitle, message: ControllerConstants.errorDialogMessage, preferredStyle: UIAlertControllerStyle.alert)
+                errorDialog.addAction(UIAlertAction(title: ControllerConstants.dialogCancelAction, style: .cancel, handler: { (_: UIAlertAction!) in
+                errorDialog.dismiss(animated: true, completion: nil)
+            }))
+            self.present(errorDialog, animated: true, completion: nil)
+        }
+
+    }
+
+    // Set chat background image
+    func setBackgroundImage(image: UIImage!) {
+        let bgView = UIImageView()
+        bgView.contentMode = .scaleAspectFill
+        bgView.image = image
+        self.collectionView?.backgroundView = bgView
+    }
+
+    // Clear chat background image
+    func clearBackgroundImage() {
+        self.collectionView?.backgroundView = nil
+    }
+
+    // Save image selected by user to user defaults
+    func saveWallpaperInUserDefaults(image: UIImage!) {
+        let imageData = UIImageJPEGRepresentation(image!, 1.0)
+        let defaults = UserDefaults.standard
+        defaults.set(imageData, forKey: userDefaultsWallpaperKey)
+    }
+
+    // Check if user defaults have an image data saved else return nil/Any
+    func getWallpaperFromUserDefaults() -> Any? {
+        let defaults = UserDefaults.standard
+        return defaults.object(forKey: userDefaultsWallpaperKey)
+    }
+
+    // Remove wallpaper from user defaults
+    func removeWallpaperFromUserDefaults() {
+        let defaults = UserDefaults.standard
+        defaults.removeObject(forKey: userDefaultsWallpaperKey)
+        clearBackgroundImage()
+    }
+
+    // Callback when image is selected from gallery
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true, completion: nil)
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        if let image = chosenImage {
+            setBackgroundImage(image: image)
+            saveWallpaperInUserDefaults(image: image)
+        }
+    }
+
+    // Callback if cancel selected from picker
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    // Show wallpaper options to set wallpaper or clear wallpaper
+    func showWallpaperOptions() {
+        let imageDialog = UIAlertController(title: ControllerConstants.wallpaperOptionsTitle, message: nil, preferredStyle: UIAlertControllerStyle.alert)
+
+        imageDialog.addAction(UIAlertAction(title: ControllerConstants.wallpaperOptionsPickAction, style: .default, handler: { (_: UIAlertAction!) in
+            imageDialog.dismiss(animated: true, completion: nil)
+            self.showImagePicker()
+        }))
+
+        imageDialog.addAction(UIAlertAction(title: ControllerConstants.wallpaperOptionsNoWallpaperAction, style: .default, handler: { (_: UIAlertAction!) in
+            imageDialog.dismiss(animated: true, completion: nil)
+            self.removeWallpaperFromUserDefaults()
+        }))
+
+        imageDialog.addAction(UIAlertAction(title: ControllerConstants.dialogCancelAction, style: .cancel, handler: { (_: UIAlertAction!) in
+            imageDialog.dismiss(animated: true, completion: nil)
+        }))
+
+        self.present(imageDialog, animated: true, completion: nil)
+    }
+
 }
 
 extension MainViewController: UITableViewDelegate {
+
+    // Enum for menu options in main chat screen
+    enum MenuOptions: Int {
+        case setttings = 0
+        case wallpaper = 1
+        case share = 2
+        case logout = 3
+    }
 
     // Handles item click
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.popover.dismiss()
 
         let index = indexPath.row
-
-        if index == 0 {
-            let settingsController = SettingsViewController(collectionViewLayout: UICollectionViewFlowLayout())
-            self.navigationController?.pushViewController(settingsController, animated: true)
-        } else if index == 3 {
-            logoutUser()
+        switch index {
+            case MenuOptions.setttings.rawValue:
+                let settingsController = SettingsViewController(collectionViewLayout: UICollectionViewFlowLayout())
+                self.navigationController?.pushViewController(settingsController, animated: true)
+                break
+            case MenuOptions.wallpaper.rawValue:
+                showWallpaperOptions()
+                break
+            case MenuOptions.logout.rawValue:
+                logoutUser()
+                break
+            default:
+                break
         }
-
     }
 
     func logoutUser() {
