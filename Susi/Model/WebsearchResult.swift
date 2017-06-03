5561a99f53f59083cd6d67eb4bd22a7c656aa2ff
Chashmeet Singh <chashmeetsingh@gmail.com>
2017-06-03 22:18:55 +0530

    fix: App is crashing sometimes when reply is websearch type

--- Susi/Model/WebsearchResult.swift
+++ Susi/Model/WebsearchResult.swift
@@ -19,23 +19,27 @@ class WebsearchResult: NSObject {
 
         if let relatedTopics = dictionary[Client.WebsearchKeys.RelatedTopics] as? [[String : AnyObject]] {
 
-            if let icon = relatedTopics[0][Client.WebsearchKeys.Icon] as? [String : String] {
-                if let image = icon[Client.WebsearchKeys.Url] {
-                    self.image = image
+            if relatedTopics.count > 0 {
+
+                if let icon = relatedTopics[0][Client.WebsearchKeys.Icon] as? [String : String] {
+                    if let image = icon[Client.WebsearchKeys.Url] {
+                        self.image = image
+                    }
                 }
-            }
 
-            if let url = relatedTopics[0][Client.WebsearchKeys.FirstURL] as? String {
-                self.url = url
-            }
+                if let url = relatedTopics[0][Client.WebsearchKeys.FirstURL] as? String {
+                    self.url = url
+                }
 
-            if let info = relatedTopics[0][Client.WebsearchKeys.Text] as? String {
-                self.info = info
-            }
+                if let info = relatedTopics[0][Client.WebsearchKeys.Text] as? String {
+                    self.info = info
+                }
 
-            if let query = dictionary[Client.WebsearchKeys.Heading] as? String {
-                let string = query.lowercased().replacingOccurrences(of: " ", with: "+")
-                self.query = string
+                if let query = dictionary[Client.WebsearchKeys.Heading] as? String {
+                    let string = query.lowercased().replacingOccurrences(of: " ", with: "+")
+                    self.query = string
+                }
+
             }
 
         }
