e240d47517684b7a07f0e1dc219ca9955196d609
Chashmeet Singh <chashmeetsingh@me.com>
2017-06-25 05:52:36 +0530

    Fix: added a minimum message width (#116)

--- Susi/Custom Views/Message Cells/OutgoingChatCell.swift
+++ Susi/Custom Views/Message Cells/OutgoingChatCell.swift
@@ -33,8 +33,8 @@ class OutgoingChatCell: ChatMessageCell {
     }
 
     func setupCell(_ estimatedFrame: CGRect, _ viewFrame: CGRect) {
-        self.messageTextView.frame = CGRect(x: viewFrame.width - estimatedFrame.width - 32, y: 0, width:estimatedFrame.width + 16, height: estimatedFrame.height + 20)
-        self.textBubbleView.frame = CGRect(x: viewFrame.width - estimatedFrame.width - 40, y: -4, width: estimatedFrame.width + 34, height: estimatedFrame.height + 26)
+        self.messageTextView.frame = CGRect(x: viewFrame.width - max(estimatedFrame.width + 26, viewFrame.width / 4 - 8), y: 0, width: max(estimatedFrame.width + 16, viewFrame.width / 4 - 16), height: estimatedFrame.height + 20)
+        self.textBubbleView.frame = CGRect(x: viewFrame.width - max(estimatedFrame.width + 32, viewFrame.width / 4), y: -4, width: max(estimatedFrame.width + 34, viewFrame.width / 4), height: estimatedFrame.height + 26)
 
         self.bubbleImageView.tintColor = UIColor.incomingMessageTintColor()
         self.messageTextView.textColor = .black
