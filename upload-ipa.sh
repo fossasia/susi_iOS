6309383c5507fcd943e530c5c010a1f5bd129cbf
Chashmeet Singh <chashmeetsingh@gmail.com>
2017-07-18 03:43:24 +0530

    fix: upload script to have .ipa file

--- upload-ipa.sh
+++ upload-ipa.sh
@@ -6,13 +6,19 @@ then
         git config --global user.email "chashmeetsingh@gmail.com"
         git clone --quiet --branch=ipa https://chashmeetsingh:$GITHUB_API_KEY@github.com/fossasia/susi_iOS ipa > /dev/null
         ls
-        rm -rf ipa/*.*
+
         cp -r $HOME/Library/Developer/Xcode/DerivedData/Susi-*/Build/Products/Debug-iphonesimulator/Susi.app ipa/Susi.app
         cd ipa
+        mkdir Payload
+        mv Susi.app Payload/
+        zip -r Payload.zip Payload
+        mv Payload.zip Susi.ipa
+        rm -rf Payload/
         ls
 
-        git checkout --orphan workaround
-        git add Susi.app
+        git rm -rf * *.* .travis.yml .github .gitignore
+        ls
+        git add .
 
         git commit -am "[Circle CI] Update Susi.app"
 
