95fbff8760a32e7d13d89b4a8f21204f87150aff
imjog <imjog24@gmail.com>
2018-05-25 15:00:37 +0530

    Initiated FiveStartRating view

---
+++ Susi/Custom Views/FiveStarRatingView/FiveStarRatingView.swift
@@ -0,0 +1,13 @@
+//
+//  FiveStarRatingView.swift
+//  Susi
+//
+//  Created by JOGENDRA on 25/05/18.
+//  Copyright © 2018 FOSSAsia. All rights reserved.
+//
+
+import UIKit
+
+class FiveStarRatingView: UIView {
+
+}
