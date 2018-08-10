08fa54ac7418d452f4c24ac2bc9309ee85244fb9
imjog <imjog24@gmail.com>
2018-08-10 21:23:20 +0530

    Integrated gravatar for users in feedback section
    resolved PR quality warnings
    
    updated cell height

--- Susi/Custom Views/Feedback Display Cell/AllFeedbackCell.swift
+++ Susi/Custom Views/Feedback Display Cell/AllFeedbackCell.swift
@@ -7,27 +7,40 @@
 //
 
 import UIKit
+import Kingfisher
 
 class AllFeedbackCell: UITableViewCell {
 
-    @IBOutlet weak var userPlaceholderLabel: UILabel!
+    @IBOutlet weak var gravatarImageView: UIImageView!
     @IBOutlet weak var userEmailLabel: UILabel!
     @IBOutlet weak var feedbackDateLabel: UILabel!
     @IBOutlet weak var userFeedbackLabel: UILabel!
 
     var feedback: Feedback? {
         didSet {
-            userPlaceholderLabel.text = feedback?.email.getFirstTwoChar().uppercased()
             userEmailLabel.text = feedback?.email
             feedbackDateLabel.text = feedback?.timeStamp.getFirstChar(10)
             userFeedbackLabel.text = feedback?.feedbackString
+            if let userEmail = feedback?.email {
+                setGravatar(from: userEmail)
+            }
             roundedCorner()
         }
     }
 
     func roundedCorner() {
-        userPlaceholderLabel.layer.cornerRadius = 22.0
-        userPlaceholderLabel.layer.masksToBounds = true
+        gravatarImageView.layer.cornerRadius = 22.0
+        gravatarImageView.layer.borderWidth = 1.0
+        gravatarImageView.layer.borderColor = UIColor.iOSGray().cgColor
+        gravatarImageView.layer.masksToBounds = true
+    }
+
+    func setGravatar(from emailString: String) {
+        let baseGravatarURL = "https://www.gravatar.com/avatar/"
+        let emailMD5 = emailString.utf8.md5.rawValue
+        let imageString = baseGravatarURL + emailMD5 + ".jpg"
+        let imageURL = URL(string: imageString)
+        gravatarImageView.kf.setImage(with: imageURL)
     }
 
 }
