858fca17d9aeaa6cb10575cd905b7b2dc42f9953
Jogendra Singh <imjog24@gmail.com>
2018-09-20 13:36:29 +0530

    hide status bar on splash screen

--- Susi/Supporting Files/AppDelegate.swift
+++ Susi/Supporting Files/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var currentUser: User?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        UIApplication.shared.isStatusBarHidden = false
         initializeRealm()
         resetStateIfUITesting()
         checkAndAssignDefaultIfFirstLaunch()
